3c47f74630f5c7b4fce6a5b4d82a9408b1e61158
Skippy <83526391+Skippy33@users.noreply.github.com>
2021-11-05 16:58:17 -0700

    changed file structure, added duplicitave kelp

---
+++ duplicativeKelp.py
@@ -0,0 +1,306 @@
+from mesa import Agent, Model, model
+from mesa import time
+from mesa.time import RandomActivation
+from mesa.space import Grid, GridContent, MultiGrid
+from mesa.datacollection import DataCollector
+from random import randint, choices, randrange, choice, shuffle
+from math import sqrt as square_root
+from time import sleep
+global killList
+killList = []
+#collects kelp data
+def kelpDataCollector(model):
+    totalKelp = 0
+    for agent in model.schedule.agents:
+        if agent.type == "kelp":
+            totalKelp += 1
+
+    return totalKelp
+
+#collects sea cow amount
+def seaCowDataCollector(model):
+    totalCows = 0
+    for agent in model.schedule.agents:
+        if agent.type == "seaCow":
+            totalCows += 1
+
+    return totalCows
+
+class MoneyModel(Model):
+    def __init__(self, kelpAmnt, seaCowAmnt, width, height):
+        #starts a grid and a scheduler
+        self.grid = MultiGrid(width, height, True)
+        self.schedule = RandomActivation(self)
+        self.modelType = "duplicative"
+        self.running = True
+
+        # Create agents
+        self.uniqueIDcounter = 0
+        for i in range(kelpAmnt):
+            self.uniqueIDcounter += 1
+            a = kelpAgent(self.uniqueIDcounter, self)
+            self.schedule.add(a)
+            # Add the agent to a random grid cell
+            x = randrange(self.grid.width)
+            y = randrange(self.grid.height)
+            #if the location selected is not emty, keep retrying till you get an empty place
+            while not self.grid.is_cell_empty([x, y]):
+                x = randrange(self.grid.width)
+                y = randrange(self.grid.height)
+            #place the agent
+            self.grid.place_agent(a, (x, y))
+
+        for j in range(seaCowAmnt):
+            #creates an agent
+            self.uniqueIDcounter += 1
+            a = seaCowAgent(self.uniqueIDcounter, self)
+            self.schedule.add(a)
+            # Add the agent to a random grid cell
+            x = randrange(self.grid.width)
+            y = randrange(self.grid.height)
+            #if the location selected is not empty, keep retrying till you get an empty place
+            while not self.grid.is_cell_empty([x, y]):
+                x = randrange(self.grid.width)
+                y = randrange(self.grid.height)
+            #place the agent
+            self.grid.place_agent(a, (x, y))
+
+        self.kelpCollector = DataCollector(
+            model_reporters={"Total Kelp Available": kelpDataCollector}
+        )
+        self.seaCowCollector = DataCollector(
+            model_reporters={"Total Sea Cows": seaCowDataCollector}
+        )
+
+    #just tells it to collect data and run scheduler every step
+    def step(self):
+
+        global killList
+        self.schedule.step()
+        #remove all dead agents
+        for x in killList:
+            self.model.grid.remove_agent(x)
+            self.model.schedule.remove(x)
+        #clear killlist
+        killList = []
+        #collect data
+        self.kelpCollector.collect(self)
+        self.seaCowCollector.collect(self)
+
+
+
+class kelpAgent(Agent):
+    def __init__(self, unique_id, model):
+        super().__init__(unique_id, model)
+        #sets type, starting food and the food timer
+        self.timeToGrow = randint(1, 7)
+        self.type = "kelp"
+        self.editable = True
+
+    def step(self):
+        #if it's full, just skip
+        #if the growing timer is up, add food. Otherwise, remove from the growing timer
+        if self.pos == None:
+            self.model.schedule.remove(self)
+            return
+        if self.timeToGrow == 0:
+            self.grow()
+        self.timeToGrow -= 1
+    
+    #what to do when it grows
+    def grow(self):
+        self.timeToGrow = randint(1, 7)
+        self.model.uniqueIDcounter += 1
+        a = kelpAgent(self.model.uniqueIDcounter, self.model)
+        #add to scheduler and creat
+        self.model.schedule.add(a)
+        possiblePlaces = self.model.grid.get_neighborhood(
+            self.pos,
+            moore=True,
+            include_center=False
+        )
+        shuffle(possiblePlaces)
+        for place in possiblePlaces:
+            if self.model.grid.is_cell_empty(place) and not self.model.grid.out_of_bounds(place) and place:
+                self.model.grid.place_agent(a, place)
+                return
+
+class seaCowAgent(Agent):
+    """ An agent with fixed initial wealth."""
+    def __init__(self, unique_id, model):
+        super().__init__(unique_id, model)
+        #sets type, starting food, age and the food timer
+        self.currentFood = randint(3,5)
+        self.type = "seaCow"
+        self.matingThreshhold = 15
+        self.age = 1
+        #self.timeToDie = choices([0,1,2,3,4,5,6,7], weights=(30, 70), k=1)[0]
+
+    #when it steps
+    def step(self):
+        print(1)
+        self.age += 1
+        if self.currentFood >= self.matingThreshhold:
+            self.matingThreshhold = 10
+            self.target = self.findMate()
+        else:
+            self.target = self.findFood()
+        #pathfind
+        self.pathfinding()
+        #if outa food, die
+        if self.currentFood <= 0:
+            self.model.grid.remove_agent(self)
+            self.model.schedule.remove(self)
+    
+    #how to pathfind using A*
+    def pathfinding(self):
+        print(2)
+        #get the nearest food or a random place to go
+
+        if type(self.target) != tuple:
+            self.targetpos = self.target.pos
+        else:
+            self.targetpos = self.target
+
+        #if there is no food, return
+        if self.target == "no food":
+            return
+
+        #if the selected kelp has no food, choose a different one
+        if type(self.target) != tuple:
+            if self.target.type == "kelp":
+                self.target = self.findFood()
+
+        #get vars for the actual A* movement
+        foodx, foody = self.targetpos
+        selfx, selfy = self.pos
+        possiblePlaces = self.model.grid.get_neighborhood(pos = self.pos, moore=True, include_center=False)
+        placesFs = []
+        placesGs = []
+
+        #get the F values for each of the possible moves
+        for neighbor in possiblePlaces:
+            
+            if self.targetpos == neighbor and type(self.target) != tuple:
+                #breeds
+                if self.target.type == "seaCow":
+                    #creates an agent
+                    self.model.uniqueIDcounter += 1
+                    a = seaCowAgent(self.model.uniqueIDcounter, self.model)
+                    #add to scheduler and creat
+                    self.model.schedule.add(a)
+                    self.model.grid.place_agent(a, self.pos)
+                    #select a new food target
+                    self.matingThreshhold = 15
+                    self.target = self.findFood()
+
+                elif self.target.type == "kelp":
+                    self.eat()
+
+
+            #get the x and y of the neighbor
+            neihborx, neighbory = neighbor
+            #get the H, G, and F values
+            H = square_root((abs(neihborx-foodx)**2 + (abs(neighbory-foody))**2))
+            G = square_root((abs(neihborx-selfx)**2 + (abs(neighbory-selfy))**2))
+            F = G + H
+            #append F to the list
+            placesFs.append(F)
+            placesGs.append(G)
+        
+        # G = cost from start to intermediate
+        # H = estimate distance to end (use pythagoras)
+        # F = G + H, estimate of how good this location is to move to
+
+        #get the coord of the best place to move to
+        minF = min(placesFs)
+        placeInList = placesFs.index(minF)
+        
+        #have it eat
+        self.currentFood -= (placesGs[placeInList]/2)
+
+        #finally move that agent, anything after this won't be ran
+        self.model.grid.move_agent(self, possiblePlaces[placeInList])
+
+
+    #identifies location of a food
+    def findFood(self):
+        i = 1
+        #while it has not spotted food
+        while i < 100:
+            print("1")
+            #continue the counter
+            #get the neighborhood as an iterable
+            neighborhood = self.model.grid.iter_neighbors(
+            pos = tuple(self.pos), 
+            include_center = False, 
+            radius = i, 
+            moore = True)
+            randomizerList = []
+            print(neighborhood)
+            #for each position in the iterable, check if it's occupied
+            for gridLocation in neighborhood:
+                print(3)
+                #if empty, pass over it
+                if self.model.grid.is_cell_empty(gridLocation.pos):
+                    pass
+                #if it is, check to see if the agent is the right type
+                else:
+                    #if it is, than check if it has food
+                    if gridLocation.type == 'kelp':
+                        #if it does, than return its location
+                        randomizerList.append(gridLocation)
+
+            i += 2
+            if randomizerList:
+                shuffle(randomizerList)
+                return randomizerList[0]
+        
+
+
+            #sets a random nearby location if there's no food
+        possible_steps = self.model.grid.get_neighborhood(
+            self.pos,
+            moore=True,
+            include_center=False,
+            radius = 1
+            )
+        #says that movement has already happened if there's no food
+        i += 2
+        return choice(possible_steps)
+
+
+    def findMate(self):
+        i = 0
+        #while it has not spotted food
+        while i < 10:
+            #continue the counter
+            i += 2
+            #get the neighborhood as an iterable
+            neighborhood = self.model.grid.iter_neighbors(pos = tuple(self.pos), 
+            include_center = False, 
+            radius = i, 
+            moore = True)
+            #for each position in the iterable, check if it's occupied
+            for gridLocation in neighborhood:
+                #if empty, pass over it
+                if self.model.grid.is_cell_empty(gridLocation.pos):
+                    pass
+                #if it is, check to see if the agent is the right type
+                else:
+                    #if it is, than check if it has food
+                    if gridLocation.type == 'seaCow':
+                        #if it does, than return its location
+                        if gridLocation.currentFood > 15:
+                            return gridLocation
+                        else:
+                            pass
+        return self.findFood()
+
+
+    
+    def eat(self):
+        #add all food from plant to self, set the kelp to have no food
+        self.currentFood += 1
+        self.model.grid.remove_agent(self.target)
+        self.model.schedule.remove(self.target)
---
+++ seaCowAgent.py
@@ -0,0 +1,208 @@
+from mesa import Agent
+from random import randint, choice, shuffle
+from math import sqrt as square_root
+
+
+
+class seaCowAgent(Agent):
+    """ An agent with fixed initial wealth."""
+    def __init__(self, unique_id, model):
+        super().__init__(unique_id, model)
+        #sets type, starting food, age and the food timer
+        self.currentFood = randint(4,7)
+        self.type = "seaCow"
+        self.matingThreshhold = 15
+        self.age = 1
+
+    #when it steps
+    def step(self):
+        self.age += 1
+        if self.currentFood >= self.matingThreshhold:
+            self.matingThreshhold = 10
+            self.target = self.findMate()
+        else:
+            self.target = self.findFood()
+        #pathfind
+        self.pathfinding()
+        #if outa food, die
+        if self.currentFood <= 0:
+            self.model.grid.remove_agent(self)
+            self.model.schedule.remove(self)
+    
+    #how to pathfind using A*
+    def pathfinding(self):
+        #get the nearest food or a random place to go
+
+        if type(self.target) != tuple:
+            self.targetpos = self.target.pos
+        else:
+            self.targetpos = self.target
+
+        #if there is no food, return
+        if self.target == "no food":
+            return
+
+        #if the selected kelp has no food, choose a different one
+        if type(self.target) != tuple:
+            if self.target.type == "kelp":
+                self.target = self.findFood()
+
+        #get vars for the actual A* movement
+        foodx, foody = self.targetpos
+        selfx, selfy = self.pos
+        possiblePlaces = self.model.grid.get_neighborhood(pos = self.pos, moore=True, include_center=False)
+        placesFs = []
+        placesGs = []
+
+        #get the F values for each of the possible moves
+        for neighbor in possiblePlaces:
+            
+            if self.targetpos == neighbor and type(self.target) != tuple:
+                #breeds
+                if self.target.type == "seaCow":
+                    #creates an agent
+                    self.model.uniqueIDcounter += 1
+                    a = seaCowAgent(self.model.uniqueIDcounter, self.model)
+                    #add to scheduler and creat
+                    self.model.schedule.add(a)
+                    self.model.grid.place_agent(a, self.pos)
+                    #select a new food target
+                    self.matingThreshhold = 15
+                    self.target = self.findFood()
+
+                elif self.target.type == "kelp":
+                    self.eat()
+
+
+            #get the x and y of the neighbor
+            neihborx, neighbory = neighbor
+            #get the H, G, and F values
+            H = square_root((abs(neihborx-foodx)**2 + (abs(neighbory-foody))**2))
+            G = square_root((abs(neihborx-selfx)**2 + (abs(neighbory-selfy))**2))
+            F = G + H
+            #append F to the list
+            placesFs.append(F)
+            placesGs.append(G)
+        
+        # G = cost from start to intermediate
+        # H = estimate distance to end (use pythagoras)
+        # F = G + H, estimate of how good this location is to move to
+
+        #get the coord of the best place to move to
+        minF = min(placesFs)
+        placeInList = placesFs.index(minF)
+        
+        #have it eat
+        self.currentFood -= (placesGs[placeInList]/2)
+
+        #finally move that agent, anything after this won't be ran
+        self.model.grid.move_agent(self, possiblePlaces[placeInList])
+
+
+    #identifies location of a food
+    def findFood(self):
+        i = 1
+        #while it has not spotted food
+        while i < 100:
+            #continue the counter
+            #get the neighborhood as an iterable
+            neighborhood = self.model.grid.iter_neighbors(
+            pos = tuple(self.pos), 
+            include_center = False, 
+            radius = i, 
+            moore = True)
+            randomizerList = []
+            #for each position in the iterable, check if it's occupied
+            for gridLocation in neighborhood:
+                #if empty, pass over it
+                if self.model.grid.is_cell_empty(gridLocation.pos):
+                    pass
+                #if it is, check to see if the agent is the right type
+                else:
+                    #optimizes, basically checks if the location has already been checked
+                    x, y = self.pos
+                    constraintupx = x+i
+                    constraintdownx = x-i
+                    constraintupy = y+i
+                    constraintdowny = y-i
+                    x, y = gridLocation.pos
+                    if x>constraintupx or x<constraintdownx or y>constraintupy or y<constraintdowny:
+                        pass
+                    else:
+                        #if it is, than check if it has food
+                        if gridLocation.type == 'kelp':
+                            #if it does, than return its location
+                            randomizerList.append(gridLocation)
+
+            i += 1
+            if randomizerList:
+                shuffle(randomizerList)
+                return randomizerList[0]
+        
+
+
+            #sets a random nearby location if there's no food
+        possible_steps = self.model.grid.get_neighborhood(
+            self.pos,
+            moore=True,
+            include_center=False,
+            radius = 1
+            )
+        #says that movement has already happened if there's no food
+        i += 2
+        return choice(possible_steps)
+
+
+    def findMate(self):
+        i = 0
+        #while it has not spotted food
+        while i < 10:
+            #continue the counter
+            i += 1
+            #get the neighborhood as an iterable
+            neighborhood = self.model.grid.iter_neighbors(pos = tuple(self.pos), 
+            include_center = False, 
+            radius = i, 
+            moore = True)
+            #for each position in the iterable, check if it's occupied
+
+            for gridLocation in neighborhood:
+                #if empty, pass over it
+                
+                if self.model.grid.is_cell_empty(gridLocation.pos):
+                    pass
+                #if it is, check to see if the agent is the right type
+                else:
+                    #optimizes, basically checks if the location has already been checked
+                    x, y = self.pos
+                    constraintupx = x+i
+                    constraintdownx = x-i
+                    constraintupy = y+i
+                    constraintdowny = y-i
+                    x, y = gridLocation.pos
+                    if x>constraintupx or x<constraintdownx or y>constraintupy or y<constraintdowny:
+                        pass
+
+                    else:
+                        #if it is, than check if it has food
+                        if gridLocation.type == 'seaCow':
+                            #if it does, than return its location
+                            if gridLocation.currentFood > 15:
+                                return gridLocation
+                            else:
+                                pass
+        return self.findFood()
+
+
+    
+    def eat(self):
+        #if this is the additive model, do its style of eating
+        if self.model.modelType == "additive":
+            self.currentFood += self.target.availableFood
+            self.target.availableFood = 0
+            self.target = self.findFood()
+        #elif it is the duplicative model, do its style of eating
+        elif self.model.modelType == "duplicative":
+            self.currentFood += 1
+            self.model.grid.remove_agent(self.target)
+            self.model.schedule.remove(self.target)
